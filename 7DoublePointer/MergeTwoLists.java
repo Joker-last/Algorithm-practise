56065cbfa7c2cf0da9200701d69de2eb4c417490
Joker-last <2084643886@qq.com>
2026-07-06 09:22:15 +0800

    第七天链表双指针

---
+++ 7DoublePointer/MergeTwoLists.java
@@ -0,0 +1,71 @@
+/*
+合并两个有序链表
+将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+
+示例 1：
+
+输入：l1 = [1,2,4], l2 = [1,3,4]
+输出：[1,1,2,3,4,4]
+
+示例 2：
+
+输入：l1 = [], l2 = []
+输出：[]
+
+示例 3：
+
+输入：l1 = [], l2 = [0]
+输出：[0]
+ 
+提示：
+两个链表的节点数目范围是 [0, 50]
+-100 <= Node.val <= 100
+l1 和 l2 均按 非递减顺序 排列
+*/
+// 自定义链表节点
+class ListNode {
+    int val;
+    ListNode next;
+    // 无参构造
+    ListNode() {}
+    // 单值构造（你代码 new ListNode(-1) 会用到）
+    ListNode(int val) {
+        this.val = val;
+    }
+    // 完整构造
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        // 虚拟头结点
+        ListNode dummy = new ListNode(-1), p = dummy;
+        ListNode p1 = l1, p2 = l2;
+
+        while (p1 != null && p2 != null) {
+            // 比较 p1 和 p2 两个指针
+            // 将值较小的的节点接到 p 指针
+            if (p1.val > p2.val) {
+                p.next = p2;
+                p2 = p2.next;
+            } else {
+                p.next = p1;
+                p1 = p1.next;
+            }
+            // p 指针不断前进
+            p = p.next;
+        }
+
+        if (p1 != null) {
+            p.next = p1;
+        }
+
+        if (p2 != null) {
+            p.next = p2;
+        }
+
+        return dummy.next;
+    }
+}
---
+++ 7DoublePointer/RemoveNthFromEnd.java
@@ -0,0 +1,55 @@
+/*
+删除链表的倒数第n个节点
+给你一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。
+
+示例 1：
+
+输入：head = [1,2,3,4,5], n = 2
+输出：[1,2,3,5]
+
+示例 2：
+
+输入：head = [1], n = 1
+输出：[]
+
+示例 3：
+
+输入：head = [1,2], n = 1
+输出：[1]
+
+提示：
+
+链表中结点的数目为 sz
+1 <= sz <= 30
+0 <= Node.val <= 100
+1 <= n <= sz
+*/
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        // 虚拟头结点
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        // 删除倒数第 n 个，要先找倒数第 n + 1 个节点
+        ListNode x = findFromEnd(dummy, n + 1);
+        // 删掉倒数第 n 个节点
+        x.next = x.next.next;
+        return dummy.next;
+    }
+        
+    private ListNode findFromEnd(ListNode head, int k) {
+        // 代码见上文
+        ListNode p1 = head;
+        // p1 先走 k 步
+        for (int i = 0; i < k; i++) {
+            p1 = p1.next;
+        }
+        ListNode p2 = head;
+        // p1 和 p2 同时走 n - k 步
+        while (p1 != null) {
+            p2 = p2.next;
+            p1 = p1.next;
+        }
+        // p2 现在指向第 n - k + 1 个节点，即倒数第 k 个节点
+        return p2;
+    }
+}
